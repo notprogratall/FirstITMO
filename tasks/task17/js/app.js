3f38804f43f39faf64f8c72528ea0925fd305ace
notprogratall <mazhano8aliza@gmail.com>
2024-04-16 21:39:09 +0300

    Добавлены комментарии в код; Изменено отображение элемента ввода текста

--- tasks/task17/js/app.js
+++ tasks/task17/js/app.js
@@ -1,5 +1,5 @@
 const Game = {
-    // свойства
+    // Слова и загадки
     WORDS: [
         { word: "христарадник", riddle: "Как на Руси называли просящего милостыню нищего?" },
         { word: "гоготалка", riddle: "Как раньше в некоторых частях России дети называли лошадь?" },
@@ -7,6 +7,7 @@ const Game = {
         { word: "рождество", riddle: "Когда девушка желала знать, за кого она выйдет замуж, она в Иванов день срывала розовый бутон, заворачивала его в белую бумагу и прятала до определенного праздника. В этот праздник бутон должен был оставаться свежим и ароматным как ни в чем не бывало. Тогда девушка брала его с собой в церковь. И тот, кому суждено было стать ее мужем, подходил к девушке и брал у нее цветок. Но если оказывалось, что бутон увял и потемнел, ворожба не срабатывала. До какого праздника от Иванова дня надо было хранить этот завернутый в бумагу бутон?" },
         { word: "башмак", riddle: "Из-за кочевого образа жизни скоморохов называли каликами перехожими. Что означает слово «калик»?" },
     ],
+    //Выбор элементов HTML шаблона
     gameField: document.querySelector('div[name="field"]'),
     goButton: document.querySelector('button[name="go"]'),
     checkButton: document.querySelector('button[name="check"]'),
@@ -14,13 +15,20 @@ const Game = {
     riddleText: document.querySelector('div[name="riddle"]'),
     gameOverText: document.querySelector('div > h1'),
     countField: document.querySelector('div[name="count-field"]'),
+    //Счетчик слов
     counter: 0,
+    //Счетчик попыток
     attemptsCounter: 0,
+    //Выбранное слово
     currWord: '',
+    //Слово, разделенное на массив (исп. в неск. функциях)
     wordLetters: [],
+    //Массив, записывающий верно введенные буквы с их индексами в wordLetters
     letterIndices: [],
 
     initGame: function () {
+        //Функция инициализации игры
+
         this.goButton.addEventListener("click", (event) => {
             // Обработчик события для кнопки "Начать игру"
             this.WORDS = this.WORDS.sort(() => Math.random() - 0.5);
@@ -33,6 +41,7 @@ const Game = {
             // Обработчик события для кнопки "Проверить"
             let sibling = event.target.previousElementSibling;
             this.checkLetter(sibling.value);
+            sibling.value = ''
         });
 
         this.newGameButton.addEventListener("click", (event) => {
@@ -47,13 +56,16 @@ const Game = {
     },
 
     changeDisplay: function (screen = 'screen-0', displayState = 'none') {
+        //Функция, меняющая вывод элементов разных наборов элементов ("экранов")
         container = document.getElementsByClassName(screen)
         for (let i = 0; i < container.length; i++) {
             container[i].style.display = displayState;
         }
     },
 
+
     startGame: function () {
+        //Функция начала игры (одно слово)
         if (this.counter == this.WORDS.length) {
             this.counter = 0
         }
@@ -84,13 +96,14 @@ const Game = {
             this.endGame(true)
         }
         else {
-            //Проверка буквы
+            //Проверка на неверное значение в поле (неигровое), не считается за игровую ошибку
             if (!/^[А-Я]$/.test(letter)) {
                 this.printError('Необходимо ввести одну букву русского алфавита')
                 console.log('Введено неверное значение: ' + letter);
                 return
             }
             let attemptIsFailed = true
+            //Проверка на неверное значение в поле (игровое)
             this.wordLetters.forEach((element, index) => {
                 if (letter == element) {
                     this.letterIndices.push({ index, letter });
@@ -99,8 +112,8 @@ const Game = {
                 }
             });
             this.countAttempt(attemptIsFailed)
-            
 
+            //Проверка на выигрыш
             if (this.letterIndices.length == this.wordLetters.length) {
                 this.endGame(true)
             }
@@ -110,12 +123,13 @@ const Game = {
             let node = document.querySelector('div[data-id="' + el.index + '"]');
             node.innerHTML = el.letter;
         });
-    }, 
+    },
 
     countAttempt: function (attemptIsFailed) {
+        //Проверка на игровую ошибку 
         if (attemptIsFailed) {
             this.attemptsCounter += 1
-            this.countField.innerHTML = 'Попыток: ' + ( 3 - this.attemptsCounter)
+            this.countField.innerHTML = 'Попыток: ' + (3 - this.attemptsCounter)
             if (this.attemptsCounter >= 3) {
                 this.endGame()
             }
@@ -128,7 +142,7 @@ const Game = {
 
     endGame(gameResult = false) {
         if (gameResult) {
-            this.gameOverText.innerHTML = 'Победа' 
+            this.gameOverText.innerHTML = 'Победа'
         }
         else {
             this.gameOverText.innerHTML = 'Проигрыш'
